a4e07a61f6d4c87d7f5f853e536719081c80729b
crayxn <crayxn@qq.com>
2023-05-12 15:41:02 +0800

    🐛 Fix

--- src/Message/Notice.php
+++ src/Message/Notice.php
@@ -22,7 +22,7 @@ class Notice
     public function send(array $openids, string $content, string $typ = NoticeType::APP_NOTIFY): bool
     {
         [, $status] = $this->notifySrvClient->Send(
-            (new ThirdMessage())->setUsers($openids)->setTyp($typ)->setContent($content)
+            (new ThirdMessage())->setNotify(true)->setUsers($openids)->setTyp($typ)->setContent($content)
         );
         return $status == StatusCode::OK;
     }
