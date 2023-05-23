5d1f57f8245f69b975e28f4a27fdeac7b573d5f4
crayxn <crayxn@qq.com>
2023-05-23 16:42:25 +0800

    增加通知类型

--- src/Message/NoticeType.php
+++ src/Message/NoticeType.php
@@ -22,4 +22,6 @@ class NoticeType
     const APP_SQUARE = "APP_SQUARE";
     const APP_CLOUD_DISK = "APP_CLOUD_DISK";
     const APP_APPROVAL = "APP_APPROVAL";
+    const APP_ADDRESS_BOOK = "APP_ADDRESS_BOOK"; //通讯录
+    const APP_TEAMS = "APP_TEAMS"; //组织
 }
