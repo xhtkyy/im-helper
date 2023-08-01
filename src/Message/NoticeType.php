ef93818f153327722a37be38a8c010516a1e0568
hahn <zbh_yu_jian@163.com>
2023-08-01 16:13:11 +0800

    fix:增加小秘书类型

--- src/Message/NoticeType.php
+++ src/Message/NoticeType.php
@@ -7,8 +7,7 @@ declare(strict_types=1);
 
 namespace Xhtkyy\ImHelper\Message;
 
-class NoticeType
-{
+class NoticeType {
     /**
      * APP_KNOW // 知行提醒
      * APP_SQUARE // 广场提醒
@@ -24,6 +23,6 @@ class NoticeType
     const APP_APPROVAL = "APP_APPROVAL"; //审批
     const APP_ADDRESS_BOOK = "APP_ADDRESS_BOOK"; //通讯录
     const APP_TEAMS = "APP_TEAMS"; //组织
-
     const APP_BUSINESS = "APP_BUSINESS"; //商务（业务）关系
+    const APP_SECRETARY = "APP_SECRETARY";//小秘书
 }
